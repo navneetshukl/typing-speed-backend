badd13e7f7f44ba1ba2225819522b3907ff6a762
navneetshukl <navneetshukla824@gmail.com>
2025-10-27 11:31:17 +0530

    handling error code

--- internals/interface/rest/api/handler/handle_error.go
+++ internals/interface/rest/api/handler/handle_error.go
@@ -14,6 +14,7 @@ func (h *Handler) handlerError(c *gin.Context, er *auth.ErrorStruct, logs *logs.
 	logs.Msg = er.ErrorMsg
 	err:=er.Error
 
+	// {403->login, 401->refresh token, 404->register user}
 	switch {
 	case errors.Is(err, typing.ErrInvalidUser):
 		logs.Status = http.StatusBadRequest
@@ -24,11 +25,46 @@ func (h *Handler) handlerError(c *gin.Context, er *auth.ErrorStruct, logs *logs.
 			"data":   nil,
 		})
 
+	case errors.Is(err,auth.ErrUserNotFound):
+		logs.Status = http.StatusNotFound
+		h.logsChan <- *logs
+		c.JSON(http.StatusNotFound, gin.H{
+			"error":  "user not found.please register.",
+			"status": http.StatusNotFound,
+			"data":   nil,
+		})
+
+		case errors.Is(err,auth.ErrInvalidUserDetail):
+		logs.Status = http.StatusBadRequest
+		h.logsChan <- *logs
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":  "user detail is not valid",
+			"status": http.StatusBadRequest,
+			"data":   nil,
+		})
+
+		case errors.Is(err,auth.ErrInvalidRefreshToken):
+		logs.Status = http.StatusForbidden
+		h.logsChan <- *logs
+		c.JSON(http.StatusForbidden, gin.H{
+			"error":  "user signin again",
+			"status": http.StatusForbidden,
+			"data":   nil,
+		})
+
+		case errors.Is(err,auth.ErrUserAlreadyRegistered):
+		logs.Status = http.StatusBadRequest
+		h.logsChan <- *logs
+		c.JSON(http.StatusForbidden, gin.H{
+			"error":  "user already registered",
+			"status": http.StatusForbidden,
+			"data":   nil,
+		})
+
 	default:
 		logs.Status = http.StatusInternalServerError
 		h.logsChan <- *logs
 		c.JSON(http.StatusInternalServerError, gin.H{
-
 			"error":  "something went wrong",
 			"status": http.StatusInternalServerError,
 			"data":   nil,
